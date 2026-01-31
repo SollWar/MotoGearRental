8ddc2105af0faade4d482829a8c67eb67f77c82d
Z3RG <sollwarmorgrind@gmail.com>
2026-01-31 21:42:20 +0300

    Гугл аналитика

--- app/layout.tsx
+++ app/layout.tsx
@@ -78,6 +78,13 @@ export default function RootLayout({
             });
           `}
         </Script>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-T55HC6Q4');`}
+        </Script>
         <noscript>
           <div>
             <img
@@ -86,6 +93,14 @@ export default function RootLayout({
               alt=""
             />
           </div>
+        </noscript>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T55HC6Q4"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
         </noscript>
       </body>
     </html>
